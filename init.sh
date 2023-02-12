6c980a36874bc28608fb958fd13df390bb6b2271
Matthew Slauson <matthew@slauson.io>
2023-02-11 22:37:14 -0600

    fix script

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+currentPath="`pwd`"
 for dir in aconfmgr alacritty autostart BetterDiscord btop cava gtk-2.0 gtk-3.0 gtk-4.0 hypr kitty Kvantum mako neofetch nvim OpenRGB paru qt5ct qt6ct ranger rofi swappy swaylock waybar wlogout
 do
     rm -rf ~/.config/$dir
-    ln -s ./.config/$dir ~/.config/$dir
+    ln -s $currentPath/.config/$dir ~/.config/$dir
 done
