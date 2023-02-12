7e7c740d3b657d706f2a1343cf4bfb61d384481e
Matthew Slauson <matthew@slauson.io>
2023-02-11 22:16:21 -0600

    start on script

---
+++ init.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+for dir in aconfmgr alacritty autostart BetterDiscord btop cava gtk-2.0 gtk-3.0 gtk-4.0 hypr kitty Kvantum mako neofetch nvim OpenRGB paru qt5ct qt6ct ranger rofi swappy swaylock waybar wlogout
+do
+    rm -rf ~/.config/$dir
+    symlink ./.config/$dir ~/.config/$dir
+done
