89c7116bacdb8e5f4627d03c65c1deb1da8c7214
Matthew Slauson <matthew@slauson.io>
2023-02-22 13:39:13 -0600

    update script

--- init.sh
+++ init.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+
+sudo pacman -S picom polybar
+
 currentPath="$(pwd)"
-for dir in aconfmgr alacritty autostart BetterDiscord btop cava gtk-2.0 gtk-3.0 gtk-4.0 hypr kitty Kvantum mako neofetch nvim OpenRGB paru qt5ct qt6ct ranger rofi swappy swaylock waybar wlogout
+for dir in aconfmgr alacritty autostart BetterDiscord btop cava dunst gtk-2.0 gtk-3.0 gtk-4.0 hypr i3 kitty Kvantum mako neofetch nvim OpenRGB paru polybar qt5ct qt6ct ranger rofi swappy swaylock waybar wlogout xfce
 do
     rm -rf ~/.config/$dir
     ln -s "$currentPath"/.config/$dir ~/.config/$dir
