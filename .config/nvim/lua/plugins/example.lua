16004e59074cf88fd9dbd696ceb8ac2eec168aff
Matthew Slauson <matthew@slauson.io>
2023-03-04 10:05:05 -0600

    moving stuff

--- .config/nvim/lua/plugins/example.lua
+++ .config/nvim/lua/plugins/example.lua
@@ -249,28 +249,4 @@ return {
       })
     end,
   },
-  { "catppuccin/nvim", name = "catppuccin" },
-  {
-    "VonHeikemen/lsp-zero.nvim",
-    branch = "v1.x",
-    dependencies = {
-      -- LSP Support
-      { "neovim/nvim-lspconfig" }, -- Required
-      { "williamboman/mason.nvim" }, -- Optional
-      { "williamboman/mason-lspconfig.nvim" }, -- Optional
-
-      -- Autocompletion
-      { "hrsh7th/nvim-cmp" }, -- Required
-      { "hrsh7th/cmp-nvim-lsp" }, -- Required
-      { "hrsh7th/cmp-buffer" }, -- Optional
-      { "hrsh7th/cmp-path" }, -- Optional
-      { "saadparwaiz1/cmp_luasnip" }, -- Optional
-      { "hrsh7th/cmp-nvim-lua" }, -- Optional
-
-      -- Snippets
-      { "L3MON4D3/LuaSnip" }, -- Required
-      { "rafamadriz/friendly-snippets" }, -- Optional
-    },
-  },
-  { "akinsho/flutter-tools.nvim", dependencies = "nvim-lua/plenary.nvim" },
 }
---
+++ .config/nvim/lua/plugins/flutter.lua
@@ -0,0 +1,3 @@
+return {
+  { "akinsho/flutter-tools.nvim", dependencies = "nvim-lua/plenary.nvim" },
+}
