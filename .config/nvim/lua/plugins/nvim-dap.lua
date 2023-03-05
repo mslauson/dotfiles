9a60fcbf6426e1bdf13f616ca92fe91129e4b976
Matthew Slauson <matthew@slauson.io>
2023-03-05 17:02:02 -0600

    add telescope support

--- .config/nvim/lua/plugins/nvim-dap.lua
+++ .config/nvim/lua/plugins/nvim-dap.lua
@@ -3,23 +3,6 @@ return {
     "mfussenegger/nvim-dap",
     config = function()
       local dap = require("dap")
-      dap.adapters.dart = {
-        type = "executable",
-        command = "dart",
-        -- This command was introduced upstream in https://github.com/dart-lang/sdk/commit/b68ccc9a
-        args = { "debug_adapter" },
-      }
-      dap.configurations.dart = {
-        {
-          type = "dart",
-          request = "launch",
-          name = "Launch flutter",
-          dartSdkPath = os.getenv("HOME") .. "/flutter/bin/cache/dart-sdk/",
-          flutterSdkPath = os.getenv("HOME") .. "/flutter",
-          program = "${workspaceFolder}/lib/main.dart",
-          cwd = "${workspaceFolder}",
-        },
-      }
     end,
   },
 }
