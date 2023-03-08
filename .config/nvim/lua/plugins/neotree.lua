eb929e13a526fcef82d6e8ea0feba3893140e0bb
Matthew Slauson <matthew@slauson.io>
2023-03-08 12:00:42 -0600

    lit updates

---
+++ .config/nvim/lua/plugins/neotree.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v2.x",
+    depends = { "kyazdani42/nvim-web-devicons", "nvim-lua/plenary.nvim", "MunifTanjim/nui.nvim" },
+    config = function()
+      require("neo-tree").setup({
+        source_selector = {
+          winbar = true,
+        },
+        filesystem = {
+          follow_current_file = true,
+          use_libuv_file_watcher = true,
+        },
+      })
+    end,
+  },
+}
