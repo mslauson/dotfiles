41c392742b82bec2b27ae1fa69b3ceca4fc0dd15
Matthew Slauson <matthew@slauson.io>
2023-03-09 10:15:51 -0600

    add config

--- .config/nvim/lua/config/lsp/init.lua
+++ .config/nvim/lua/config/lsp/init.lua
@@ -1,5 +1,12 @@
 local M = {}
 
+local home = vim.env.HOME
+local maven_home = vim.env.MAVEN_HOME
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+local workspace_dir = home .. "/.cache/jdtls_workspaces/" .. project_name
+local lombokjar = "/usr/lib/lombok-common/lombok.jar"
+local eclipse_java_google_style = vim.fn.stdpath("config") .. "/rules/eclipse-java-google-style.xml"
+local jars = vim.fn.stdpath("data") .. "/jars/"
 -- local util = require "lspconfig.util"
 
 local servers = {
@@ -118,7 +125,138 @@ local servers = {
       },
     },
   },
-  jdtls = {},
+  jdtls = {
+
+    -- custom script,for further configurations
+    -- cmd = { 'java-lsp', workspace_dir },
+    cmd = { "jdtls", "--jvm-arg=-javaagent:" .. lombokjar, "-data", workspace_dir },
+    root_dir = require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }),
+    -- root_dir = vim.fn.getcwd(),
+    -- filetypes =  'java',
+    settings = {
+      java = {
+        signatureHelp = { enabled = true },
+        contentProvider = { preferred = "fernflower" },
+        implementationsCodeLens = {
+          enabled = true,
+        },
+        referencesCodeLens = {
+          enabled = true,
+        },
+        templates = {
+          fileHeader = {
+            "/**",
+            " * @author: ${user}",
+            " * @date: ${date}",
+            " * @description: ${file_name}",
+            " */",
+          },
+          typeComment = {
+            "/**",
+            " * @author: ${user}",
+            " * @date: ${date}",
+            " * @description: ${type_name}",
+            " */",
+          },
+        },
+        import = {
+          maven = { enabled = true },
+          exclusions = {
+            "**/node_modules/**",
+            "**/.metadata/**",
+            "**/archetype-resources/**",
+            "**/META-INF/maven/**",
+            "**/Frontend/**",
+            "**/CSV_Aggregator/**",
+          },
+        },
+        maven = {
+          downloadSources = true,
+          updateSnapshots = true,
+        },
+        autobuild = { enabled = true },
+        completion = {
+          favoriteStaticMembers = {
+            "org.hamcrest.MatcherAssert.assertThat",
+            "org.hamcrest.Matchers.*",
+            "org.hamcrest.CoreMatchers.*",
+            "org.junit.jupiter.api.Assertions.*",
+            "java.util.Objects.requireNonNull",
+            "java.util.Objects.requireNonNullElse",
+            "org.mockito.Mockito.*",
+          },
+          overwrite = false,
+          guessMethodArguments = true,
+        },
+        sources = {
+          organizeImports = {
+            starThreshold = 9999,
+            staticStarThreshold = 9999,
+          },
+        },
+        codeGeneration = {
+          generateComments = true,
+          useBlocks = true,
+          hashCodeEquals = {
+            userIntanceOf = true,
+          },
+          toString = {
+            template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}",
+            codeStyle = "STRING_BUILDER_CHAINED",
+          },
+        },
+        configuration = {
+          maven = {
+            globalSettings = maven_home .. "/conf/settings.xml",
+            userSettings = home .. "/.local/repos/.m2/settings.xml",
+          },
+          runtimes = {
+            {
+              name = "JavaSE-1.8",
+              path = "/usr/lib/jvm/java-18-jdk/",
+            },
+            {
+              name = "JavaSE-11",
+              path = "/usr/lib/jvm/11.0.6.j9-adpt/",
+            },
+            { name = "JavaSE-17", path = vim.env.JAVA_HOME },
+            {
+              name = "JavaSE-18",
+              path = "/usr/lib/jvm/java-18-jdk/",
+            },
+          },
+        },
+        format = {
+          settings = {
+            url = eclipse_java_google_style,
+            profile = "GoogleStyle",
+          },
+        },
+      },
+    },
+    init_options = {
+      bundles = bundles,
+      extendedClientCapabilities = extendedClientCapabilities,
+    },
+
+    flags = {
+      allow_incremental_sync = true,
+      debounce_text_changes = 150,
+      server_side_fuzzy_completion = true,
+    },
+    capabilities = {
+      workspace = {
+        configuration = true,
+      },
+      textDocument = {
+        completion = {
+          completionItem = {
+            snippentSupport = true,
+          },
+        },
+      },
+    },
+  },
   dockerls = {},
   -- graphql = {},
   bashls = {},
@@ -178,20 +316,20 @@ function M.on_attach(client, bufnr)
 
   -- Configure for jdtls
   if client.name == "jdt.ls" then
-    require("jdtls").setup_dap { hotcodereplace = "auto" }
+    require("jdtls").setup_dap({ hotcodereplace = "auto" })
     require("jdtls.dap").setup_dap_main_class_configs()
     vim.lsp.codelens.refresh()
   end
 
   -- nvim-navic
   if caps.documentSymbolProvider then
-    local navic = require "nvim-navic"
+    local navic = require("nvim-navic")
     navic.attach(client, bufnr)
   end
 
   if client.name ~= "null-ls" then
     -- inlay-hints
-    local ih = require "inlay-hints"
+    local ih = require("inlay-hints")
     ih.on_attach(client, bufnr)
 
     -- semantic highlighting -- https://github.com/neovim/neovim/pull/21100
@@ -259,13 +397,13 @@ function M.toggle_diagnostics()
 end
 
 function M.remove_unused_imports()
-  vim.diagnostic.setqflist { severity = vim.diagnostic.severity.WARN }
-  vim.cmd "packadd cfilter"
-  vim.cmd "Cfilter /main/"
-  vim.cmd "Cfilter /The import/"
-  vim.cmd "cdo normal dd"
-  vim.cmd "cclose"
-  vim.cmd "wa"
+  vim.diagnostic.setqflist({ severity = vim.diagnostic.severity.WARN })
+  vim.cmd("packadd cfilter")
+  vim.cmd("Cfilter /main/")
+  vim.cmd("Cfilter /The import/")
+  vim.cmd("cdo normal dd")
+  vim.cmd("cclose")
+  vim.cmd("wa")
 end
 
 return M
