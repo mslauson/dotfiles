3763d45ce1abe05291a3c2d1920a7ab37379a254
Matthew Slauson <matthew@slauson.io>
2023-03-10 16:15:27 -0600

    update

--- .config/nvim/lua/plugins/cmp-kitty.lua
+++
@@ -1,9 +0,0 @@
-local init = require("mason-lspconfig.server_configurations.bicep")
-return {
-  {
-    "garyhurtz/cmp_kitty",
-    init = function()
-      require("cmp_kitty"):setup()
-    end,
-  },
-}
