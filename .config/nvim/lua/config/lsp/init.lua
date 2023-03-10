5ee0168750bc8b636b03e7dd3c9c396dded3b032
Matthew Slauson <matthew@slauson.io>
2023-03-10 09:25:07 -0600

    hopefully this helps

--- .config/nvim/lua/config/lsp/init.lua
+++ .config/nvim/lua/config/lsp/init.lua
@@ -129,133 +129,133 @@ local servers = {
 
     -- custom script,for further configurations
     -- cmd = { 'java-lsp', workspace_dir },
-    cmd = { "jdtls", "--jvm-arg=-javaagent:" .. lombokjar, "-data", workspace_dir },
-    root_dir = require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }),
-    -- root_dir = vim.fn.getcwd(),
-    -- filetypes =  'java',
-    settings = {
-      java = {
-        signatureHelp = { enabled = true },
-        contentProvider = { preferred = "fernflower" },
-        implementationsCodeLens = {
-          enabled = true,
-        },
-        referencesCodeLens = {
-          enabled = true,
-        },
-        templates = {
-          fileHeader = {
-            "/**",
-            " * @author: ${user}",
-            " * @date: ${date}",
-            " * @description: ${file_name}",
-            " */",
-          },
-          typeComment = {
-            "/**",
-            " * @author: ${user}",
-            " * @date: ${date}",
-            " * @description: ${type_name}",
-            " */",
-          },
-        },
-        import = {
-          maven = { enabled = true },
-          exclusions = {
-            "**/node_modules/**",
-            "**/.metadata/**",
-            "**/archetype-resources/**",
-            "**/META-INF/maven/**",
-            "**/Frontend/**",
-            "**/CSV_Aggregator/**",
-          },
-        },
-        maven = {
-          downloadSources = true,
-          updateSnapshots = true,
-        },
-        autobuild = { enabled = true },
-        completion = {
-          favoriteStaticMembers = {
-            "org.hamcrest.MatcherAssert.assertThat",
-            "org.hamcrest.Matchers.*",
-            "org.hamcrest.CoreMatchers.*",
-            "org.junit.jupiter.api.Assertions.*",
-            "java.util.Objects.requireNonNull",
-            "java.util.Objects.requireNonNullElse",
-            "org.mockito.Mockito.*",
-          },
-          overwrite = false,
-          guessMethodArguments = true,
-        },
-        sources = {
-          organizeImports = {
-            starThreshold = 9999,
-            staticStarThreshold = 9999,
-          },
-        },
-        codeGeneration = {
-          generateComments = true,
-          useBlocks = true,
-          hashCodeEquals = {
-            userIntanceOf = true,
-          },
-          toString = {
-            template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}",
-            codeStyle = "STRING_BUILDER_CHAINED",
-          },
-        },
-        configuration = {
-          maven = {
-            globalSettings = maven_home .. "/conf/settings.xml",
-            userSettings = home .. "/.local/repos/.m2/settings.xml",
-          },
-          runtimes = {
-            {
-              name = "JavaSE-1.8",
-              path = "/usr/lib/jvm/java-18-jdk/",
-            },
-            {
-              name = "JavaSE-11",
-              path = "/usr/lib/jvm/11.0.6.j9-adpt/",
-            },
-            { name = "JavaSE-17", path = vim.env.JAVA_HOME },
-            {
-              name = "JavaSE-18",
-              path = "/usr/lib/jvm/java-18-jdk/",
-            },
-          },
-        },
-        format = {
-          settings = {
-            url = eclipse_java_google_style,
-            profile = "GoogleStyle",
-          },
-        },
-      },
-    },
-    init_options = {
-      bundles = bundles,
-      extendedClientCapabilities = extendedClientCapabilities,
-    },
+    --  cmd = { "jdtls", "--jvm-arg=-javaagent:" .. lombokjar, "-data", workspace_dir },
+    --  root_dir = require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }),
+    --  -- root_dir = vim.fn.getcwd(),
+    --  -- filetypes =  'java',
+    --  settings = {
+    --    java = {
+    --      signatureHelp = { enabled = true },
+    --      contentProvider = { preferred = "fernflower" },
+    --      implementationsCodeLens = {
+    --        enabled = true,
+    --      },
+    --      referencesCodeLens = {
+    --        enabled = true,
+    --      },
+    --      templates = {
+    --        fileHeader = {
+    --          "/**",
+    --          " * @author: ${user}",
+    --          " * @date: ${date}",
+    --          " * @description: ${file_name}",
+    --          " */",
+    --        },
+    --        typeComment = {
+    --          "/**",
+    --          " * @author: ${user}",
+    --          " * @date: ${date}",
+    --          " * @description: ${type_name}",
+    --          " */",
+    --        },
+    --      },
+    --      import = {
+    --        maven = { enabled = true },
+    --        exclusions = {
+    --          "**/node_modules/**",
+    --          "**/.metadata/**",
+    --          "**/archetype-resources/**",
+    --          "**/META-INF/maven/**",
+    --          "**/Frontend/**",
+    --          "**/CSV_Aggregator/**",
+    --        },
+    --      },
+    --      maven = {
+    --        downloadSources = true,
+    --        updateSnapshots = true,
+    --      },
+    --      autobuild = { enabled = true },
+    --      completion = {
+    --        favoriteStaticMembers = {
+    --          "org.hamcrest.MatcherAssert.assertThat",
+    --          "org.hamcrest.Matchers.*",
+    --          "org.hamcrest.CoreMatchers.*",
+    --          "org.junit.jupiter.api.Assertions.*",
+    --          "java.util.Objects.requireNonNull",
+    --          "java.util.Objects.requireNonNullElse",
+    --          "org.mockito.Mockito.*",
+    --        },
+    --        overwrite = false,
+    --        guessMethodArguments = true,
+    --      },
+    --      sources = {
+    --        organizeImports = {
+    --          starThreshold = 9999,
+    --          staticStarThreshold = 9999,
+    --        },
+    --      },
+    --      codeGeneration = {
+    --        generateComments = true,
+    --        useBlocks = true,
+    --        hashCodeEquals = {
+    --          userIntanceOf = true,
+    --        },
+    --        toString = {
+    --          template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}",
+    --          codeStyle = "STRING_BUILDER_CHAINED",
+    --        },
+    --      },
+    --      configuration = {
+    --        maven = {
+    --          globalSettings = maven_home .. "/conf/settings.xml",
+    --          userSettings = home .. "/.local/repos/.m2/settings.xml",
+    --        },
+    --        runtimes = {
+    --          {
+    --            name = "JavaSE-1.8",
+    --            path = "/usr/lib/jvm/java-18-jdk/",
+    --          },
+    --          {
+    --            name = "JavaSE-11",
+    --            path = "/usr/lib/jvm/11.0.6.j9-adpt/",
+    --          },
+    --          { name = "JavaSE-17", path = vim.env.JAVA_HOME },
+    --          {
+    --            name = "JavaSE-18",
+    --            path = "/usr/lib/jvm/java-18-jdk/",
+    --          },
+    --        },
+    --      },
+    --      format = {
+    --        settings = {
+    --          url = eclipse_java_google_style,
+    --          profile = "GoogleStyle",
+    --        },
+    --      },
+    --    },
+    --  },
+    --  init_options = {
+    --    bundles = bundles,
+    --    extendedClientCapabilities = extendedClientCapabilities,
+    --  },
 
-    flags = {
-      allow_incremental_sync = true,
-      debounce_text_changes = 150,
-      server_side_fuzzy_completion = true,
-    },
-    capabilities = {
-      workspace = {
-        configuration = true,
-      },
-      textDocument = {
-        completion = {
-          completionItem = {
-            snippentSupport = true,
-          },
-        },
-      },
-    },
+    --  flags = {
+    --    allow_incremental_sync = true,
+    --    debounce_text_changes = 150,
+    --    server_side_fuzzy_completion = true,
+    --  },
+    --  capabilities = {
+    --    workspace = {
+    --      configuration = true,
+    --    },
+    --    textDocument = {
+    --      completion = {
+    --        completionItem = {
+    --          snippentSupport = true,
+    --        },
+    --      },
+    --    },
+    --  },
   },
   dockerls = {},
   -- graphql = {},
