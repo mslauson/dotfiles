23861af335e00e248b7ba0dde88b05bff65dd7f3
Matthew Slauson <matthew@slauson.io>
2023-03-12 00:44:37 -0600

    somewhat better? IDK

--- .config/nvim/lua/astronvim/lazy.lua
+++ .config/nvim/lua/astronvim/lazy.lua
@@ -1,7 +1,8 @@
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   -- stylua: ignore
-  vim.fn.system { "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath }
+  vim.fn.system { "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
+    lazypath }
   local oldcmdheight = vim.opt.cmdheight:get()
   vim.opt.cmdheight = 1
   vim.notify "Please wait while plugins are installed..."
@@ -31,14 +32,14 @@ local colorscheme = astronvim.default_colorscheme and { astronvim.default_colors
 require("lazy").setup(astronvim.user_opts("lazy", {
   spec = spec,
   defaults = {
-      -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-      -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
-      lazy = false,
-      -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
-      -- have outdated releases, which may break your Neovim install.
-      version = false, -- always use the latest git commit
-      -- version = "*", -- try installing the latest stable version for plugins that support semver
-    },
+    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
+    lazy = false,
+    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+    -- have outdated releases, which may break your Neovim install.
+    version = false, -- always use the latest git commit
+    -- version = "*", -- try installing the latest stable version for plugins that support semver
+  },
   install = { colorscheme = colorscheme },
   performance = {
     rtp = {
