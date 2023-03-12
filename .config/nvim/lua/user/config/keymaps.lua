1e7b47d145cb1ebafb5c660f58cf67ca17cd7454
Matthew Slauson <matthew@slauson.io>
2023-03-12 00:29:00 -0600

    oops

---
+++ .config/nvim/lua/user/config/keymaps.lua
@@ -0,0 +1,65 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps hereby
+local keymap = vim.keymap.set
+local opts = { silent = true }
+local dap_ok, dap = pcall(require, "dap")
+local dap_ui_ok, ui = pcall(require, "dapui")
+
+if not (dap_ok and dap_ui_ok) then
+  require("notify")("nvim-dap or dap-ui not installed!", "warning") -- nvim-notify is a separate plugin, I recommend it too!
+  return
+end
+
+-- flutter maps
+keymap("n", "<leader>flo", "<cmd>FlutterOutlineToggle<cr>", { desc = "Toggle Flutter Outline" })
+keymap("n", "<leader>fld", "<cmd>FlutterDevices<cr>", { desc = "Toggle Flutter Devices" })
+keymap("n", "<leader>fle", "<cmd>FlutterEmulators<cr>", { desc = "Toggle Flutter Emulators" })
+keymap("n", "<leader>flrs", "<cmd>FlutterRestart<cr>", { desc = "Flutter Restart" })
+keymap("n", "<leader>flrl", "<cmd>FlutterReload<cr>", { desc = "Flutter Reload" })
+keymap("n", "<leader>flq", "<cmd>FlutterQuit<cr>", { desc = "Flutter Quit" })
+
+-- dap maps
+vim.fn.sign_define("DapBreakpoint", { text = "🐞" })
+
+-- Start debugging session
+keymap("n", "<localleader>ds", function()
+  dap.continue()
+  ui.toggle({})
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-w>=", false, true, true), "n", false) -- Spaces buffers evenly
+end)
+
+-- Set breakpoints, get variable values, step into/out of functions, etc.
+keymap("n", "<localleader>dh", require("dap.ui.widgets").hover, { desc = "Show variable value" })
+keymap("n", "<localleader>dc", dap.continue, { desc = "Continue" })
+keymap("n", "<localleader>db", dap.toggle_breakpoint, { desc = "Toggle breakpoint" })
+keymap("n", "<localleader>dn", dap.step_over, { desc = "Step over" })
+keymap("n", "<localleader>di", dap.step_into, { desc = "Step into" })
+keymap("n", "<localleader>do", dap.step_out, { desc = "Step out" })
+keymap("n", "<localleader>dC", function()
+  dap.clear_breakpoints()
+  require("notify")("Breakpoints cleared", "warn")
+end, { desc = "Clear breakpoints" })
+
+-- Close debugger and clear breakpoints
+keymap("n", "<localleader>de", function()
+  dap.clear_breakpoints()
+  ui.toggle({})
+  dap.terminate()
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-w>=", false, true, true), "n", false)
+  require("notify")("Debugger session ended", "warn")
+end, { desc = "End debugger session" })
+
+-- dap-ui-maps
+
+-- keymap("n", "<leader>dt", ui.toggle(), { desc = "Toggle DAP UI" })
+
+-- neotree
+keymap("n", "<leader>ntrf", "<cmd>Neotree left %:p:h:h %:p<cr>", { desc = "NeoTree Reveal File" })
+keymap("n", "<leader>ntsb", "<cmd>Neotree float buffers<cr>", { desc = "NeoTree Show Buffers" })
+keymap("n", "<leader>ntcb", "<cmd>Neotree close buffers<cr>", { desc = "NeoTree Close Buffers" })
+keymap("n", "<leader>ntsg", "<cmd>Neotree float git_status<cr>", { desc = "NeoTree Show Git Status" })
+keymap("n", "<leader>ntcb", "<cmd>Neotree close git_status<cr>", { desc = "NeoTree Close Git Status" })
+
+-- rnvimr
+keymap("n", "<leader>ro", "<cmd>RnvimrToggle<cr>", { desc = "NeoTree Close Git Status" })
---
+++ .config/nvim/lua/user/plugins/flutter.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "akinsho/flutter-tools.nvim",
+    dependencies = "nvim-lua/plenary.nvim",
+    config = function()
+      require("flutter-tools").setup({
+        experimental = {
+          lsp_derive_paths = true,
+        },
+        debugger = {
+          enabled = true,
+          run_via_dap = true,
+        },
+        outline = { auto_open = false },
+        decorations = {
+          statusline = { device = true, app_version = true },
+        },
+        widget_guides = { enabled = true, debug = true },
+        dev_log = { enabled = false, open_cmd = "tabedit" },
+        lsp = {
+          color = {
+            enabled = true,
+            background = true,
+            virtual_text = false,
+          },
+          settings = {
+            showTodos = true,
+            renameFilesWithClasses = "prompt",
+          },
+          on_attach = require("user.config.lsp").on_attach,
+          capabilities = require("user.config.lsp").capabilities,
+        },
+      })
+    end,
+  },
+}
